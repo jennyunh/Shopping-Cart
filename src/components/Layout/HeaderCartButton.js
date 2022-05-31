9b4e96e676ca4d9494deaf0730a0d62b2406e9cf
jennyunh <jennyunh@gmail.com>
2022-05-31 15:15:16 -0700

    added the bounce effect whenever item is added to cart. Incorporated useEffect to add animation, and setTimeout to remove animation. Cleanup function for setTimeout as also included.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../store/cartContext';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from './CartIcon';
 
 const HeaderCartButton = props => {
+
+const [btnHighlight, setBtnHighlight] = useState(false);
+
 const cartContext = useContext(CartContext);
 
 //.reduce() takes 2 arguments. First argument is a function, and the second argument is a starting value.
 const numOfCartItems = cartContext.items.reduce((current, item) => {return current + item.amount;}, 0);
 
+const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ''}`;
+
+const { items } = cartContext;
+
+useEffect(() => {
+    if (items.length === 0) {
+        return}
+
+setBtnHighlight(true);
+
+const timer = setTimeout(() => {setBtnHighlight(false)}, 300);
+
+
+//return a cleanup function.
+return () => {clearTimeout(timer)};
+
+}, [items]);
+
+
+
+
 return (
-<button className={classes.button} onClick={props.onClick}>
+<button className={btnClasses} onClick={props.onClick}>
 <span className={classes.icon}>
     <CartIcon/>
 </span>
